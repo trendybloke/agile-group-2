ba60256da9f712bfa0c9cfe5f2063157bd625847
trendybloke <joshprice141@gmail.com>
2022-12-07 10:31:22 +0000

    Written basic unit tests for yfinance

--- django_environment/django-venv/Django_Environment/etfs/tests.py
+++ django_environment/django-venv/Django_Environment/etfs/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+import yfinance as yf
 
-# Create your tests here.
+class yfinanceDataTest(TestCase):
+    """ Pulls an ETF, confirms that the data is correct """
+    def setUp(self):
+        """ Create ETF info dict. """
+        self.etf_info = yf.Ticker("BAB").info
+        pass
+    
+    def test_symbol_correct(self):
+        """ Has the right ETF been pulled? """
+        self.assertEqual(self.etf_info["symbol"], "BAB")
+    
--- django_environment/django-venv/Django_Environment/etfs/views.py
+++ django_environment/django-venv/Django_Environment/etfs/views.py
@@ -7,6 +7,26 @@ from django.core.exceptions import ObjectDoesNotExist
 from accounts.models import ETF
 import yfinance as yf
 
+def buildContext(etf_details):
+    context = {}
+    # context["etf_data"] = etf_details
+    
+    context["etf_symbol"] = etf_details["symbol"]
+    context["etf_long_name"] = etf_details["longName"]
+    context["etf_summary"] = etf_details["longBusinessSummary"]
+    context["etf_logo"] = etf_details["logo_url"]
+    
+    if(etf_details["quoteType"] == "ETF"):
+        # Add ETF only data here
+        context["etf_price"] = str(round(etf_details["regularMarketPrice"], 2)) + " " + etf_details["currency"] 
+        context["etf_amount"] = etf_details["askSize"]
+    else:    
+        # Add stock only data here
+        context["etf_price"] = str(round(etf_details["currentPrice"], 2)) + " " + etf_details["currency"] 
+        context["etf_website"] = etf_details["website"]
+
+    return context
+
 def etf_browse(request):
     """
     Request for ETF browse page
@@ -26,25 +46,24 @@ def etf_browse(request):
 
         etf_companies[etf.symbol] = etf_info["longName"]
         
-        growth_data = str()
         price_data = str()
         price = str()
 
         etf_quotetypes[etf.symbol] = etf_info["quoteType"]
         
         if(etf_info["quoteType"] == "ETF"):
-            growth_data = "revenueQuarterlyGrowth"
             price_data = "regularMarketPrice"
+            growth = round(etf_info[price_data] - etf_info["previousClose"], 2)
         else:
-            growth_data = "revenueGrowth"
             price_data = "currentPrice"
+            growth = etf_info["revenueGrowth"]
     
-        growth = etf_info[growth_data]
+        # growth = etf_info[growth_data]
         
         if growth and growth > 0:
             growth = "+ " + str(growth)
         
-        price = str(etf_info[price_data]) + " " + etf_info["currency"]
+        price = str(round(etf_info[price_data], 2)) + " " + etf_info["currency"]
     
         etf_growths[etf.symbol] =  growth
         etf_prices[etf.symbol] = price
@@ -61,28 +80,13 @@ def etf_details(request, etf_symbol):
     """
     Request for ETF details page
     """
-    context = {}
-
     try:
         requestedEtf = ETF.objects.get(symbol = etf_symbol)
-        context["etf_symbol"] = requestedEtf.symbol
-        
+
         # Dictionary containing up to date info on the passed ETF
-        etf_details = yf.Ticker(etf_symbol).info
-        
-        context["etf_data"] = etf_details
-        context["etf_long_name"] = etf_details["longName"]
-        context["etf_summary"] = etf_details["longBusinessSummary"]
-        context["etf_logo"] = etf_details["logo_url"]
-        
-        if(etf_details["quoteType"] == "ETF"):
-            # Add ETF only data here
-            context["etf_price"] = str(etf_details["regularMarketPrice"]) + " " + etf_details["currency"] 
-            context["etf_amount"] = etf_details["askSize"]
-        else:    
-            # Add stock only data here
-            context["etf_price"] = str(etf_details["currentPrice"]) + " " + etf_details["currency"] 
-            context["etf_website"] = etf_details["website"]
+        etf_details = yf.Ticker(requestedEtf.symbol).info
+
+        context = buildContext(etf_details)
         
         return render(request, "etf_details.html", context)
     # Not found; redirect to browse
@@ -97,19 +101,11 @@ def purchase_etf(request, etf_symbol):
     
     try:
         requestedEtf = ETF.objects.get(symbol = etf_symbol)
-        context["etf_symbol"] = requestedEtf.symbol
-        
-        # Dictionary containing up to date infor on the passed ETF
-        etf_details = yf.Ticker(etf_symbol).info
-        
-        context["etf_long_name"] = etf_details["longName"]
-        context["etf_logo"] = etf_details["logo_url"]
-        
-        if(etf_details["quoteType"] == "ETF"):
-            context["etf_price"] = str(etf_details["regularMarketPrice"]) + " " + etf_details["currency"] 
-            context["etf_amount"] = etf_details["askSize"]
-        else:
-            context["etf_price"] = str(etf_details["currentPrice"]) + etf_details["currency"]
+
+        # Dictionary containing up to date info on the passed ETF
+        etf_details = yf.Ticker(requestedEtf.symbol).info
+
+        context = buildContext(etf_details)
         
         return render(request, "purchase_etf.html", context)
     # Not found; redirect to browse
