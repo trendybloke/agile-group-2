71377a20db551b46a8f951d5dfca43ec47779d3c
trendybloke <joshprice141@gmail.com>
2022-12-07 09:39:10 +0000

    Misc. Fixes

--- django_environment/django-venv/Django_Environment/etfs/views.py
+++ django_environment/django-venv/Django_Environment/etfs/views.py
@@ -22,29 +22,33 @@ def etf_browse(request):
     for etf in etfs:
         etf_info = yf.Ticker(etf.symbol).info
         
+        # print("\n\n" + str(etf_info))
+
         etf_companies[etf.symbol] = etf_info["longName"]
         
         growth_data = str()
         price_data = str()
-        
+        price = str()
+
         etf_quotetypes[etf.symbol] = etf_info["quoteType"]
         
         if(etf_info["quoteType"] == "ETF"):
             growth_data = "revenueQuarterlyGrowth"
-            price_data = "ask"
+            price_data = "regularMarketPrice"
         else:
             growth_data = "revenueGrowth"
             price_data = "currentPrice"
     
         growth = etf_info[growth_data]
-        price = str(etf_info[price_data]) + " " + etf_info["currency"]
         
         if growth and growth > 0:
             growth = "+ " + str(growth)
+        
+        price = str(etf_info[price_data]) + " " + etf_info["currency"]
     
         etf_growths[etf.symbol] =  growth
         etf_prices[etf.symbol] = price
-        
+
     context["etf_list"] = etfs
     context["etf_companies"] = etf_companies.items()
     context["etf_growths"] = etf_growths.items()
@@ -69,16 +73,16 @@ def etf_details(request, etf_symbol):
         context["etf_data"] = etf_details
         context["etf_long_name"] = etf_details["longName"]
         context["etf_summary"] = etf_details["longBusinessSummary"]
+        context["etf_logo"] = etf_details["logo_url"]
         
         if(etf_details["quoteType"] == "ETF"):
             # Add ETF only data here
-            context["etf_price"] = str(etf_details["ask"]) + " " + etf_details["currency"] 
+            context["etf_price"] = str(etf_details["regularMarketPrice"]) + " " + etf_details["currency"] 
             context["etf_amount"] = etf_details["askSize"]
         else:    
             # Add stock only data here
             context["etf_price"] = str(etf_details["currentPrice"]) + " " + etf_details["currency"] 
             context["etf_website"] = etf_details["website"]
-            context["etf_logo"] = etf_details["logo_url"]
         
         return render(request, "etf_details.html", context)
     # Not found; redirect to browse
@@ -99,13 +103,13 @@ def purchase_etf(request, etf_symbol):
         etf_details = yf.Ticker(etf_symbol).info
         
         context["etf_long_name"] = etf_details["longName"]
+        context["etf_logo"] = etf_details["logo_url"]
         
         if(etf_details["quoteType"] == "ETF"):
-            context["etf_price"] = str(etf_details["ask"]) + " " + etf_details["currency"] 
+            context["etf_price"] = str(etf_details["regularMarketPrice"]) + " " + etf_details["currency"] 
             context["etf_amount"] = etf_details["askSize"]
         else:
             context["etf_price"] = str(etf_details["currentPrice"]) + etf_details["currency"]
-            context["etf_logo"] = etf_details["logo_url"]
         
         return render(request, "purchase_etf.html", context)
     # Not found; redirect to browse
