a7c126235984dc9b44f5a9c3ec02f28c691efaba
trendybloke <joshprice141@gmail.com>
2022-12-09 23:33:10 +0000

    Users can purchase ETFs from the browsing interface.

--- django_environment/django-venv/Django_Environment/etfs/urls.py
+++ django_environment/django-venv/Django_Environment/etfs/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('browse', views.etf_browse, name='browse_etfs'),
     path('details/<etf_symbol>/', views.etf_details, name='etf_details'),
     path('purchase/<etf_symbol>/', views.purchase_etf, name='purchase_etf'),
+    path('purchase/new/<etf_symbol>/', views.instantiate_etf, name='instantiate_etf')
 ]
