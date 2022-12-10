88f0d908072bac2ae67081982904cfdf2d53f314
aliceop1 <61065238+aliceop1@users.noreply.github.com>
2022-12-10 17:33:55 +0000

    Bootstrap, css, login/2fa merged
    Login routes for 2FA now fixed using the correct 'manager'. Bootstrap home/login/signups complete

--- django_environment/django-venv/Django_Environment/accounts/urls.py
+++ django_environment/django-venv/Django_Environment/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from accounts import views
 from .views import SignUpView
+from .views import register_user
 
 urlpatterns = [ 
     path("signup/", SignUpView.as_view(), name="signup"),
     path('portfolio/<username>/', views.portfolio, name='portfolio'),
-    path('portfolio/<username>/sell', views.sell_etf, name='sell_etf')
+    path('portfolio/<username>/sell', views.sell_etf, name='sell_etf'),
+    path('register/', register_user, name="register"),
 ]
